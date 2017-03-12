7523ee4fe5841a8cd0389d8923fc6cd226138730
David Guichon <davidrguichon@gmail.com>
2017-03-12 14:01:53 -0400

    Initial Commit

---
+++ Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by David Guichon on 2017-03-12.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
